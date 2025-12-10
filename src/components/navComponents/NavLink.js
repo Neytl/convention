fb529e2e6d91ed0479ecd3910217b5bb1f9acd41
nrr56436 <58192898+nrr56436@users.noreply.github.com>
2025-12-09 21:08:05 -0600

    pathing fix for nav links

--- src/components/navComponents/NavLink.js
+++ src/components/navComponents/NavLink.js
@@ -1,9 +1,14 @@
 import SimpleImage from "convention/components/generalComponents/SimpleImage";
 import "convention/app/css/nav.css";
+import nextConfig from "convention/../next.config.mjs";
+const basePath = nextConfig.basePath;
 
 export default function NavLink({ name, href, iconSrc, currentPage }) {
   return (
-    <a className={currentPage ? "navLink currentPage" : "navLink"} href={href}>
+    <a
+      className={currentPage ? "navLink currentPage" : "navLink"}
+      href={basePath + href}
+    >
       <SimpleImage src={iconSrc} width={20} height={20} />
       <span>{name}</span>
     </a>
